954398210bc0d34b5bf9428c5449762cf0ff5ae6
sourabhrathi146-dot <sourabhrathi146@gmail.com>
2026-07-20 02:31:31 +0530

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "household-ledger-v19";
+const CACHE_NAME = "household-ledger-v20";
 const ASSETS = ["./", "./index.html", "./manifest.json", "./icon.svg"];
 
 self.addEventListener("install", (event) => {
